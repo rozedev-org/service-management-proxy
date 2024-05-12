a2dca09a5817e387175b63031ffe5283b25aaa00
Juan C Jimenez <juancarlo14071@gmail.com>
2024-05-11 21:07:19 -0400

    chore: Update Dockerfile and Kubernetes configuration
    This commit updates the Dockerfile and Kubernetes configuration files to expose port 80 instead of port 5000. This change aligns with the standard port for HTTP traffic and ensures compatibility with other services.

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,5 @@ FROM nginx:1.25.3-alpine
 COPY ./nginx.conf /etc/nginx/conf.d/default.conf
 
 
-EXPOSE 5000
+EXPOSE 80
 
--- cicd/cicd.sh
+++ cicd/cicd.sh
@@ -23,9 +23,9 @@ echo "Empuje de la imagen de Docker completado con éxito."
 
 # Desplegar en Kubernetes
 echo "Iniciando despliegue en Kubernetes..."
-kubectl apply -k ../k8s/remote || handle_error "despliegue 1"
-kubectl set image deployment/$appName $appName=$image -n alerti || handle_error "despliegue 2"
-kubectl rollout restart deployment/$appName -n alerti || handle_error "despliegue 3"
+kubectl apply -k ../k8s || handle_error "despliegue 1"
+kubectl set image deployment/$appName $appName=$image -n service-management || handle_error "despliegue 2"
+kubectl rollout restart deployment/$appName -n service-management || handle_error "despliegue 3"
 echo "Despliegue en Kubernetes completado con éxito."
 
 echo "Todas las acciones se completaron con éxito."
